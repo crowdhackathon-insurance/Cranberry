2c7a9d150cc61916369ec4f3f1cd7eea5daa50e5
linux <linux@smartboy.coom>
2016-10-01 19:50:20 +0300

    creation of models

---
+++ lumen/app/Vehicle_issue.php
@@ -0,0 +1,8 @@
+<?php namespace App;
+ 
+use Illuminate\Database\Eloquent\Model;
+ 
+class Vehicle_issue extends Model{
+    
+
+}
---
+++ lumen/app/Vehicles_gps.php
@@ -0,0 +1,8 @@
+<?php namespace App;
+ 
+use Illuminate\Database\Eloquent\Model;
+ 
+class Vehicle_gps extends Model{
+    
+protected $table = 'Vehicle_gps';
+}
