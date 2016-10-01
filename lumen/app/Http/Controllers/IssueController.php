1870718dbb62f953ff04fbf05923f1f27c172e0a
linux <linux@smartboy.coom>
2016-10-02 02:58:17 +0300

    setup getissue/{id}

--- lumen/app/Http/Controllers/IssueController.php
+++ lumen/app/Http/Controllers/IssueController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers;
  
 use App\Issue;
+use App\Customer;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
- 
+use DB;
  
 class IssueController extends Controller{
  
@@ -24,8 +25,25 @@ class IssueController extends Controller{
      * @return void
      */
     public function getIssue($id){
-    	$response='hello world '.$id;
-       return $response;
+    	
+        $issue  = new Issue;
+        // $issue_results=$issue::find($id);
+        $issue_results = DB::table('issues')
+                                ->select('issues.status','statuses.label')
+                                ->leftjoin('statuses', 'statuses.id', '=', 'issues.status')
+                                ->where('issues.id',$id)
+                                ->limit(1)
+                                ->get();
+
+
+
+        $ret_val=array(
+            "id"=>$issue_results[0]->status,
+            "label"=>$issue_results[0]->label
+            );
+        $json_ret_value=json_encode($ret_val);
+
+       return $json_ret_value;
     }
 
 
